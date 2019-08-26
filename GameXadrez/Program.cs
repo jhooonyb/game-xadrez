d0266318f75bcf8aba838a77e073396bf01bc4e7
Jhonata Buenyo <jhonata_rjd@hotmail.com>
2019-08-25 23:29:24 -0300

    adicionando tabuleiro

--- GameXadrez/Program.cs
+++ GameXadrez/Program.cs
@@ -7,11 +7,7 @@ namespace GameXadrez
     {
         static void Main(string[] args)
         {
-            Posicao P;
-
-            P = new Posicao(3, 4);
-
-            Console.WriteLine("Posicao:! "+P);
+            Tabuleiro tab = new Tabuleiro(8, 8);
         }
     }
 }
---
+++ GameXadrez/tabuleiro/Cor.cs
@@ -0,0 +1,10 @@
+﻿namespace tabuleiro
+{
+    enum Cor
+    {
+        Branca,
+        Preta,
+        Azul,
+        Vermelho
+    }
+}
