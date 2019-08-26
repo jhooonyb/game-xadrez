b9ba16102d79bd90fba40d9dad31cba156321bba
Jhonata Buenyo <jhonata_rjd@hotmail.com>
2019-08-25 23:17:34 -0300

    criando classe posicao

--- GameXadrez/Program.cs
+++ GameXadrez/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using tabuleiro;
 
 namespace GameXadrez
 {
@@ -6,7 +7,11 @@ namespace GameXadrez
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posicao:! "+P);
         }
     }
 }
